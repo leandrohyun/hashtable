c4db79339a6d26b26109ed29bd37d8ab0e9e5c98
Leandro Hyun <leandro.souza@icen.ufpa.br>
2022-05-24 20:15:20 -0300

    hashtable

---
+++ hashtable.js
@@ -0,0 +1,9 @@
+/*
+Hash table será usado para armazenar os dados, nesse caso, 
+o número de pacientes em cada hospital ou clínica espalhados pela cidade.
+
+Além disso, esse método poderá ser utilizado em outros serviços, como bombeiros,
+ polícia e transporte, cada um com seus dados próprios sendo importante para um 
+ bom gerenciamento dentro da cidade jogada.
+
+*/
